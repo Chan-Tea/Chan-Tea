c6c45b2214f1cb637a41ac6b709c1c2426f2e030
Chan-Tea <baodz200x@gmail.com>
2026-08-28 13:11:56 +0700

    feat: add public Supabase configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CHANTEA_CONFIG = {
+  supabaseUrl: "https://opzzmvlpvdexmdljdsvm.supabase.co",
+  supabaseKey: "sb_publishable_OEvaxdOAUn9VJgpcpn3BhA_q7oIFOvF",
+  imageBucket: "menu-images"
+};
